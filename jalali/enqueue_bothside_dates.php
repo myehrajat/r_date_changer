d092aac24e9a03d21db0eadb7aeb6a768a155795
myehrajat <31361589+myehrajat@users.noreply.github.com>
2018-12-08 18:28:30 +0330

    moment/locale/fa.js
    no need to enqueue this file because of moment is add locale version

--- jalali/enqueue_bothside_dates.php
+++ jalali/enqueue_bothside_dates.php
@@ -5,7 +5,6 @@ if(get_post_type( $_GET['post'] )=='product' or $_GET['post_type']== 'product' )
 add_action( 'wp_enqueue_scripts', 'RentIt_Date_Changer_enqueue_scripts_bothside',9999 );
 function RentIt_Date_Changer_enqueue_scripts_bothside() {
 
-	wp_enqueue_script( 'renita_moment-fa',plugins_url("moment/locale/fa.js",dirname(__FILE__) ),array('renita_moment-with-locales'),  '0.8.1', true );
 	wp_enqueue_script( 'renita_moment-jalaali',plugins_url("moment-jalaali/build/moment-jalaali.js",__FILE__ ),array('renita_moment-with-locales'),  '0.8.1', true );
 	wp_enqueue_script( 'renita_pDate', plugins_url("datetimepicker/bootstrap-datetimepicker-persian.min.js",__FILE__ ), array('renita_moment-jalaali'),'1', true );
 	wp_enqueue_script( 'renita_settings_pDate', plugins_url("datetimepicker/settings.js",__FILE__ ), array('renita_pDate'),'0.0.6', true );
